cf48912ce9d3590f945d090654498e4797f8a5a0
Dionnes <dionnes2323@gmail.com>
2025-04-26 14:37:39 -0300

    Criação do projeto java socket

---
+++ src/main/java/com/sd/dds_funcionario_socket_thread/Dds_Funcionario_Socket_Thread.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sd.dds_funcionario_socket_thread;
+
+/**
+ *
+ * @author Dionnes
+ */
+public class Dds_Funcionario_Socket_Thread {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
