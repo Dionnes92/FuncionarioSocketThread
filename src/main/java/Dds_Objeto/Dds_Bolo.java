fbe7e9f25187a14a66820a9254cd71deb10aa669
Dionnes <dionnes2323@gmail.com>
2025-04-26 17:09:40 -0300

    Criação da class and object bolo

---
+++ src/main/java/Dds_Objeto/Dds_Bolo.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dds_Objeto;
+
+/**
+ *
+ * @author Dionnes
+ */
+public class Dds_Bolo {
+    
+    private String Dds_NomeBolo;
+    private String Dds_tipoBolo;
+    private String Dds_Recheio;
+    private String Dds_Tamanho;
+    private String Dds_Valor;
+    private String Dds_Data;
+
+    public String getDds_NomeBolo() {
+        return Dds_NomeBolo;
+    }
+
+    public void setDds_NomeBolo(String Dds_NomeBolo) {
+        this.Dds_NomeBolo = Dds_NomeBolo;
+    }
+
+    public String getDds_tipoBolo() {
+        return Dds_tipoBolo;
+    }
+
+    public void setDds_tipoBolo(String Dds_tipoBolo) {
+        this.Dds_tipoBolo = Dds_tipoBolo;
+    }
+
+    public String getDds_Recheio() {
+        return Dds_Recheio;
+    }
+
+    public void setDds_Recheio(String Dds_Recheio) {
+        this.Dds_Recheio = Dds_Recheio;
+    }
+
+    public String getDds_Tamanho() {
+        return Dds_Tamanho;
+    }
+
+    public void setDds_Tamanho(String Dds_Tamanho) {
+        this.Dds_Tamanho = Dds_Tamanho;
+    }
+
+    public String getDds_Valor() {
+        return Dds_Valor;
+    }
+
+    public void setDds_Valor(String Dds_Valor) {
+        this.Dds_Valor = Dds_Valor;
+    }
+
+    public String getDds_Data() {
+        return Dds_Data;
+    }
+
+    public void setDds_Data(String Dds_Data) {
+        this.Dds_Data = Dds_Data;
+    }
+    
+    
+    
+}
